31ce7152ca7e362b49c3a87bb7c2627be73f9d2f
Daan Middendorp <github-d@landgenoot.com>
2018-07-09 12:08:41 +0200

    Convert to proper NPM module

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let kLib = require('./lib/kAnonymity.js')
+let lLib = require('./lib/lDiversity.js')
+
+module.exports.kAnonymity = kLib.kAnonymity
+module.exports.lDiversity = lLib.lDiversity
